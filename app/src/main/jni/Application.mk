218258d75311e99060ffaa0d269f9e8d14d5e986
hkucs-makerlab <makerlab@cs.hku.hk>
2021-09-27 10:59:34 +0800

    fix ndk-build; correct the way to include OpenCV library in AAR format for debug and release apk; output different APKs based on different ABI; set a basename for ouput APKs

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_STL := c++_static
+APP_STL := c++_shared
 APP_CPPFLAGS := -frtti -fexceptions -std=gnu++11 -Wno-format-contains-nul -g -Wno-deprecated-declarations
-APP_CPPFLAGS +=-fpermissive
+APP_CPPFLAGS += -fpermissive
 APP_ABI := all
